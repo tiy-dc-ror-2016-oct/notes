b619fff09b2729b9a92eff2f6e6faa659ead2589
Russell Osborne <russell@burningpony.com>
2016-10-11 10:19:09 -0400

    First pass at pbj

---
+++ 1/1 monday/cli_examples.sh
@@ -0,0 +1,69 @@
+Last login: Mon Oct 10 18:44:15 on ttys002
+~ $ pwd
+/Users/rposborne
+~ $ ls
+Applications            Music                   exercism
+Desktop                 Pictures                github
+Documents               Public                  journal.txt
+Downloads               RubymineProjects        org
+Dropbox (Personal)      VirtualBox VMs          theironyard
+Dropbox (The Iron Yard) code                    websites
+Library                 dotfiles
+Movies                  eclipse
+~ $ cd Dropbox\ \(The\ Iron\ Ya
+~ $ cd Desktop/
+~/Desktop $ ls
+19839647.png                  slack-imgs.com.jpeg
+August Transit Credit TIY.pdf t8.shakespeare
+class
+~/Desktop $ ls
+19839647.png                  class
+August Transit Credit TIY.pdf slack-imgs.com.jpeg
+Terminal Demo                 t8.shakespeare
+~/Desktop $ mkdir from_the_terminal
+~/Desktop $ cd ..
+~ $ open .
+~ $ open ..
+~ $ open Desktop/class/1/
+1 monday/    2 tuesday/   3 wedensday/ 4 thursday/
+~ $ open Desktop/class/1/
+1 monday/    2 tuesday/   3 wedensday/ 4 thursday/
+~ $ open Desktop/class/1/1\ monday/
+~ $ open Desktop/class/
+.DS_Store  1/         3/         5/         7/         9/
+.git/      2/         4/         6/         8/         setup/
+~ $ open Desktop/class/
+.DS_Store  1/         3/         5/         7/         9/
+.git/      2/         4/         6/         8/         setup/
+~ $ open Desktop/class/setup/make_folders.rb
+~ $ ls
+Applications            Music                   exercism
+Desktop                 Pictures                github
+Documents               Public                  journal.txt
+Downloads               RubymineProjects        org
+Dropbox (Personal)      VirtualBox VMs          theironyard
+Dropbox (The Iron Yard) code                    websites
+Library                 dotfiles
+Movies                  eclipse
+~ $ ls
+Applications            Music                   exercism
+Desktop                 Pictures                github
+Documents               Public                  journal.txt
+Downloads               RubymineProjects        org
+Dropbox (Personal)      VirtualBox VMs          theironyard
+Dropbox (The Iron Yard) code                    websites
+Library                 dotfiles
+Movies                  eclipse
+~ $ cd Desktop/
+~/Desktop $ ls
+19839647.png                  from_the_terminal
+August Transit Credit TIY.pdf slack-imgs.com.jpeg
+Terminal Demo                 t8.shakespeare
+class
+~/Desktop $ cd class/
+~/Desktop/class (master) $ ls
+1     2     3     4     5     6     7     8     9     setup
+~/Desktop/class (master) $ cd 1/1\ monday/
+~/Desktop/class/1/1 monday (master) $ ls
+~/Desktop/class/1/1 monday (master) $ open .
+~/Desktop/class/1/1 monday (master) $ atom .
