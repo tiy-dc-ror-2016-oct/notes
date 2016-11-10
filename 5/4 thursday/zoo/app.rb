492169582c482a68a47aee1cd3934fe972803a18
Russell Osborne <russell@burningpony.com>
2016-11-10 10:35:38 -0500

    Added a new endpoint, styled button.
    New endpoint is reuses the template we created to handle the error state
    when creating a animal. The button is pretty.

--- 5/4 thursday/zoo/app.rb
+++ 5/4 thursday/zoo/app.rb
@@ -1,15 +1,18 @@
 require "sinatra"
 require "./db/connection"
 
+# Homepage
 get "/" do
   erb :"home/index.html", layout: :"layout/application.html"
 end
 
+# List all Animals
 get "/animals" do
   @animals = Animal.all
   erb :"animals/index.html", layout: :"layout/application.html"
 end
 
+# Create Animals
 post "/animals" do
   @animal = Animal.new(params)
   if @animal.save
@@ -17,4 +20,9 @@ post "/animals" do
   else
     erb :"animals/new.html", layout: :"layout/application.html"
   end
+end
+
+get "/animals/new" do
+  @animal = Animal.new
+  erb :"animals/new.html", layout: :"layout/application.html"
 end
