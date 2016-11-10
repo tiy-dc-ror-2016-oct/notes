2c52a74d4bf9f450dd387ddf50247383c1c014a8
Russell Osborne <russell@burningpony.com>
2016-11-10 10:58:39 -0500

    Add a delete button and route, turned on method override

--- 5/4 thursday/zoo/app.rb
+++ 5/4 thursday/zoo/app.rb
@@ -1,6 +1,8 @@
 require "sinatra"
 require "./db/connection"
 
+set :method_override, true
+
 # Homepage
 get "/" do
   erb :"home/index.html", layout: :"layout/application.html"
@@ -25,4 +27,9 @@ end
 get "/animals/new" do
   @animal = Animal.new
   erb :"animals/new.html", layout: :"layout/application.html"
+end
+
+delete "/animals/:id" do
+  Animal.find(params["id"]).destroy
+  redirect "/animals"
 end
