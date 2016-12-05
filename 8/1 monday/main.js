51c9894d563e9ded3b32ff537cb4cb145b1e9442
Russell Osborne <russell@burningpony.com>
2016-12-05 13:03:33 -0500

    Adding notes for JS

---
+++ 8/1 monday/main.js
@@ -0,0 +1,48 @@
+var widgets = 3;
+widgets;
+console.log(widgets);
+console.log(1+1);
+console.log(1/3);
+
+function lengthOfString(str) {
+  return str.length;
+}
+
+
+var mooseNameLength = null;
+console.log(mooseNameLength);
+mooseNameLength = lengthOfString("moose");
+console.log(mooseNameLength);
+
+
+var students = ["ben","allie","alex"];
+
+console.log(students[0]);
+
+
+students.forEach(function logger (student){
+   console.log(student);
+});
+
+var newObj = {
+  name: "Russell",
+  yell: function yell() {
+    alert("Hi");
+  }
+
+};
+console.log(newObj.name);
+console.log(newObj.yell);
+// console.log(newObj.yell());
+
+if ( 1 == 1 ){
+  console.log("Truthy");
+} else if (2 == 2){
+
+}
+
+
+
+stuents.forEach(function(){
+
+});
