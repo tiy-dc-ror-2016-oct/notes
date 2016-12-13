ff0de6ad3be97279386d42cdc4873a9274e97660
Russell Osborne <russell@burningpony.com>
2016-12-13 10:52:11 -0500

    Adding subject

--- 9/2 tuesday/spec/card_spec.rb
+++ 9/2 tuesday/spec/card_spec.rb
@@ -1,16 +1,13 @@
 require_relative "../card"
 
 RSpec.describe Card do
-  
-  before(:each) do
-    @card = Card.new("A", :spades)
-  end
+  subject(:card) { described_class.new("A", :spades) }
 
   it "has a face" do
-    expect(@card.face).to eq("A")
+    expect(card.face).to eq("A")
   end
 
   it "has a suit" do
-    expect(@card.suit).to eq(:spades)
+    expect(card.suit).to eq(:spades)
   end
 end
